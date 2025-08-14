e80c6826909dee4e02ba7455e7b7357903176386
Jack <raxmatoveldor37@gmail.com>
2025-08-14 12:27:07 +0500

    tb_boards_settings was created successfully!

---
+++ root/db/pgsql/migrations/20250814062622_create_tb_boards_settings.ts
@@ -0,0 +1,57 @@
+import type {Knex} from "knex";
+import {config} from '../../../config/config';
+
+const {
+    DB_DATA: {
+        PGSQL: {
+            TABLES: {
+                TB_BOARDS,
+                TB_BOARDS_SETTINGS
+            }
+        }
+    }
+} = config;
+
+export async function up(knex: Knex): Promise<void> {
+    try {
+        console.log(`Creating ${TB_BOARDS_SETTINGS} table...`);
+
+        await knex.schema.createTable(TB_BOARDS_SETTINGS, (t) => {
+            t.increments('id').primary();
+            t.integer('board_id').notNullable().unique()
+                .references('id').inTable(TB_BOARDS)
+                .onDelete('CASCADE')
+                .notNullable();
+            t.string('color', 24).defaultTo(null);
+            t.boolean('allow_swimlanes').notNullable().defaultTo(false);
+            t.jsonb('wip_limits').notNullable().defaultTo('{}'); // {columnId: limit}
+            t.boolean('auto_archive_done').notNullable().defaultTo(false);
+            t.integer('auto_archive_days').defaultTo(null); // if enabled
+            t.jsonb('meta').notNullable().defaultTo('{}'); // board-level custom fields
+            t.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+            t.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+        });
+
+        // this is trigger for updated_at colum in tb_users
+        await knex.raw(`
+            CREATE TRIGGER update_${TB_BOARDS_SETTINGS}_updated_at
+            BEFORE UPDATE ON ${TB_BOARDS_SETTINGS}
+            FOR EACH ROW
+            EXECUTE FUNCTION set_updated_at();
+        `);
+
+        console.log(`Created ${TB_BOARDS_SETTINGS} table successfully!!!`);
+    } catch (e) {
+        console.error(`Error creating ${TB_BOARDS_SETTINGS} table:`, e);
+    }
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    try {
+        await knex.schema.dropTableIfExists(TB_BOARDS_SETTINGS);
+    } catch (e) {
+        console.error(`Error creating ${TB_BOARDS_SETTINGS} table:`, e);
+    }
+}
+
