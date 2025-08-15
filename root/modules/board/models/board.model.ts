24896a4b3308fd7af6c657f9a86e7c9af7809185
Jack <raxmatoveldor37@gmail.com>
2025-08-15 12:35:12 +0500

    To formed board model using by transaction for create

--- root/modules/board/models/board.model.ts
+++ root/modules/board/models/board.model.ts
@@ -1,6 +1,8 @@
 import type {FastifyInstance} from 'fastify';
 import {config} from "../../../config/config";
 
+import {IBoard} from "../interfaces/board.interface";
+
 import {CreateBoardDto} from "../dto/create.board.dto";
 
 const {
@@ -15,7 +17,19 @@ export class BoardModel {
     constructor(protected fastify: FastifyInstance) {
     }
 
-    async create(createBoardDto: CreateBoardDto): Promise<object> {
-        return {};
+    async create(createBoardDto: CreateBoardDto): Promise<IBoard | undefined> {
+        const transaction = await this.fastify.pgsql.transaction();
+        try {
+            const board = await transaction(TB_BOARDS).insert(createBoardDto)
+                .returning('*')
+                .then((rows) => rows[0]);
+
+            await transaction.commit();
+
+            return board;
+        } catch (e) {
+            await transaction.rollback();
+            throw e;
+        }
     }
 }
