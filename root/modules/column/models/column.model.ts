eaba5bfeeb1ac7be87e3ec4a72124a5da55bb1aa
Jack <raxmatoveldor37@gmail.com>
2025-08-22 14:01:52 +0500

    Get column changed to get column tasks.

--- root/modules/column/models/column.model.ts
+++ root/modules/column/models/column.model.ts
@@ -11,7 +11,8 @@ const {
     DB_DATA: {
         PGSQL: {
             TABLES: {
-                TB_BOARDS_COLUMNS
+                TB_BOARDS_COLUMNS,
+                TB_COLUMNS_TASKS,
             }
         }
     }
@@ -27,12 +28,102 @@ export class ColumnModel {
     }
 
     async readOne(query: Partial<QueryColumnDto>): Promise<IColumn | undefined> {
-        return this.fastify.pgsql(TB_BOARDS_COLUMNS).select("*")
+        return this.fastify.pgsql(`${TB_BOARDS_COLUMNS} as cl`)
+            .select([
+                'cl.*',
+                this.fastify.pgsql.raw(`COALESCE(clt.tasks, '[]'::jsonb) as tasks`),
+            ])
+            .leftJoin(this.fastify.pgsql.raw(`
+             LATERAL (
+                SELECT jsonb_agg(
+                         jsonb_build_object(
+                           'id', clt.id,
+                           
+                           'title', clt.title,
+                           'desc', clt.desc,
+                           
+                           'priority_id', clt.priority_id,
+                           
+                           'start_date', clt.start_date,
+                           'due_date', clt.due_date,
+                           'completed_at', clt.completed_at,
+                           
+                           'order_rank', clt.order_rank,
+                           
+                           'external_refs', clt.external_refs,
+                           'contact', clt.contact,
+                           'has_lead', clt.has_lead,
+                           'lead_id', clt.lead_id,
+                           
+                           'labels', clt.labels,
+                           'attachments', clt.attachments,
+                           'checklist', clt.checklist,
+                           
+                           'made_by', clt.made_by,
+                           'updated_by', clt.updated_by,
+                           
+                           'meta', clt.meta,
+                           
+                           'is_active', clt.is_active,
+                           'updated_at', clt.updated_at,
+                           'created_at', clt.created_at
+                         )
+                         ORDER BY clt.order_rank
+                       ) AS tasks
+                FROM ${TB_COLUMNS_TASKS} clt
+                WHERE clt.column_id = cl.id
+             ) clt ON TRUE
+            `))
             .where(query).first();
     }
 
     async readAll(query: Partial<QueryColumnDto>): Promise<IColumn[]> {
-        return this.fastify.pgsql(TB_BOARDS_COLUMNS).select("*")
+        return this.fastify.pgsql(`${TB_BOARDS_COLUMNS} as cl`)
+            .select([
+                'cl.*',
+                this.fastify.pgsql.raw(`COALESCE(clt.tasks, '[]'::jsonb) as tasks`),
+            ])
+            .leftJoin(this.fastify.pgsql.raw(`
+             LATERAL (
+                SELECT jsonb_agg(
+                         jsonb_build_object(
+                           'id', clt.id,
+                           
+                           'title', clt.title,
+                           'desc', clt.desc,
+                           
+                           'priority_id', clt.priority_id,
+                           
+                           'start_date', clt.start_date,
+                           'due_date', clt.due_date,
+                           'completed_at', clt.completed_at,
+                           
+                           'order_rank', clt.order_rank,
+                           
+                           'external_refs', clt.external_refs,
+                           'contact', clt.contact,
+                           'has_lead', clt.has_lead,
+                           'lead_id', clt.lead_id,
+                           
+                           'labels', clt.labels,
+                           'attachments', clt.attachments,
+                           'checklist', clt.checklist,
+                           
+                           'made_by', clt.made_by,
+                           'updated_by', clt.updated_by,
+                           
+                           'meta', clt.meta,
+                           
+                           'is_active', clt.is_active,
+                           'updated_at', clt.updated_at,
+                           'created_at', clt.created_at
+                         )
+                         ORDER BY clt.order_rank
+                       ) AS tasks
+                FROM ${TB_COLUMNS_TASKS} clt
+                WHERE clt.column_id = cl.id
+             ) clt ON TRUE
+            `))
             .where(query);
     }
 
