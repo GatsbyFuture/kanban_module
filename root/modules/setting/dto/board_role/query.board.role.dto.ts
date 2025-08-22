e0145fb0c4db6f237e9670e3ccd0ec1f15ebd56d
Jack <raxmatoveldor37@gmail.com>
2025-08-22 15:07:40 +0500

    The board model CRUD was created successfully!

---
+++ root/modules/setting/dto/board_role/query.board.role.dto.ts
@@ -0,0 +1,5 @@
+export interface QueryBoardRoleDto {
+    readonly id: number;
+    readonly code: string;
+    readonly name: string;
+}
--- root/modules/setting/models/setting.model.ts
+++ root/modules/setting/models/setting.model.ts
@@ -4,6 +4,7 @@ import {config} from '../../../config/config';
 import {IBoardRole} from "../interfaces/setting.interface";
 
 import {CreateBoardRoleDto} from "../dto/board_role/create.board.role.dto";
+import {QueryBoardRoleDto} from "../dto/board_role/query.board.role.dto";
 
 const {
     DB_DATA: {
@@ -24,5 +25,18 @@ export class SettingModel {
             .returning('*').then(rows => rows[0]);
     }
 
-    // async getOne()
+    async getOne(query: QueryBoardRoleDto): Promise<IBoardRole> {
+        return this.fastify.pgsql(TB_BOARDS_ROLES).select('*')
+            .where(query).first();
+    }
+
+    async getAll(query: QueryBoardRoleDto): Promise<IBoardRole[]> {
+        return this.fastify.pgsql(TB_BOARDS_ROLES).select('*')
+            .where(query);
+    }
+
+    async deleteMany(ids: number[]): Promise<Partial<IBoardRole>[]> {
+        return this.fastify.pgsql(TB_BOARDS_ROLES).whereIn('id', ids)
+            .returning(['code', 'name']).del();
+    }
 }
